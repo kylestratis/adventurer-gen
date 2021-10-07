50fdc32a67aa6ca09b2d824fe4d2604216933697
kylestratis <kyle.stratis@gmail.com>
2021-10-06 22:13:20 -0400

    Initial scaffolding

---
+++ contracts/AdventurerGen.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.8.0;
+
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+import { Base64 } from "./libraries/Base64.sol";
+
+// We inherit the contract we imported. This means we'll have access
+// to the inherited contract's methods.
+contract AdventurerNFT is ERC721URIStorage {
+}
